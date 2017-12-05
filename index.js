2ce93f0be8c338d8712e91bdaf863ba978046c39
Gerson Calienes <gersoncalienes@gmail.com>
2017-12-05 00:03:32 -0500

    added passport and google oAuth modules

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-//GET route handler\\\\
-app.get('/', (req, res) => {
-  res.send({ hi: 'there, Gerson' });
-});
+passport.use(new GoogleStrategy());
+
+// //GET route handler\\\\
+// app.get('/', (req, res) => {
+//   res.send({ hi: 'there, Gerson' });
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
