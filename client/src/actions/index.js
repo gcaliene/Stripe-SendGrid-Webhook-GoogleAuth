c2e4c93327140f47fd51461a1cbfe2397ae10da1
Gerson Calienes <gersoncalienes@gmail.com>
2017-12-09 13:14:07 -0500

    auth route competed

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
-  return function(dispatch) {
-    //redux thunk will see a return function and call the dispatch
-    axios
-      .get('/api/current_user')
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+//Using promises
+// export const fetchUser = () => {
+//   return function(dispatch) {
+//     //redux thunk will see a return function and call the dispatch
+//     axios
+//       .get('/api/current_user')
+//       .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//   };
+// };
+
+//Using es2017 with await/async
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res.data }); //this is the action going to the authReducer
 };
+
+//refactored even further
+// export const fetchUser = () => async dispatch =>
+//   dispatch({type: FETCH_USER, payload: await axios.get('/api/current_user')});
